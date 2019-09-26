439634241b23e5681f95da9ef3b33144b744bd11
Murali Karunanithy <murali@Muralis-MacBook-Pro-2.local>
2019-09-26 07:04:37 +0100

    first commit

---
+++ src/main/java/com/murali/me/App.java
@@ -0,0 +1,13 @@
+package com.murali.me;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
